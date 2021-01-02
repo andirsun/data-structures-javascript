185e20f9e276071948e8b6dc3fcdff537ffae49e
Anderson Laverde Gracia <anderson.laverde@zohomail.com>
2021-01-02 12:58:52 -0500

    hashtables : get, delete Keys

--- hashTable.js
+++ hashTable.js
@@ -27,17 +27,58 @@ class HashTable {
   }
 
   set (key, value) {
-    const address = this.hasMethod(key);
+    const address = this.hasMethod (key);
     // If the address no exists, then create a array to set values
     if (!this.data[address]) {
       this.data[address] = [];
     }
     // Set the new items
-    this.data[address].push([key, value]);
+    this.data[address].push ([key, value]);
     return this.data;
   }
 
-  
+  get (key) {
+    const address = this.hasMethod (key);
+    const currentBucket = this.data[address];
+    if (currentBucket) {
+      // Loop in the array of sub-arrays
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] == key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  delete (key) { 
+    const address = this.hasMethod (key);
+    const currentBucket = this.data[address];
+    if (currentBucket) {
+      // Loop in the array of sub-arrays
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] == key) {
+          // delete the key value array
+          delete currentBucket[i];
+          return "deleted";
+        }
+      }
+    }
+    return "No exists";
+  }
+
+  getAllKeys () {
+    let keys = [];
+    this.data.forEach((item) => {
+      item.forEach((tuple) => {
+        keys.push(tuple[0]);
+      })
+    })
+    if (keys.length == 0) {
+      return "No Keys";
+    }
+    return keys;
+  }
 }
 
 const myHashTable = new HashTable(50);
